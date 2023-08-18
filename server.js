ebf059e698d3e5b20eb13d5af6db0ccc3f1f93d3
anggawanridho <anggawanridho@gmail.com>
2023-08-19 00:44:34 +0700

    pelengkapan fitur

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const db = require('./models');
 const prompt = require('prompt-sync')({sigint: true});
 const controller = require('./controller');
-// const page = require('./pages');
 
 (async () => {
     try {
@@ -100,12 +99,84 @@ const controller = require('./controller');
                     }
                     if (Number(invetoryMenu) === 2) {
                         // list stock
+                        let listBarang = await controller.item.showAll();
+                        console.log(`\n`);
+                        console.log(`|  No.  |   ID Barang   |       Nama Barang       |  Kategori  |     Harga    | Jumlah Stok |    Masuk   |   Kadaluarsa  |  Distributor | Lokasi Stok |         Deskripsi         |`);
+                        console.log(`-`.repeat(179)); // baris pembatas
+
+                        listBarang.forEach((barang, index) => {
+                            const formattedIndex = index + 1;
+                            const { idBarang, namaBarang, kategoriBarang, harga, jumlahStok, tanggalMasuk, tanggalKadaluarsa, distributor, lokasiStok, deskripsiBarang } = barang.dataValues;
+                            
+                            console.log(`|  ${formattedIndex.toString().padEnd(4)} | ${idBarang.padEnd(13)} | ${namaBarang.padEnd(24)} | ${kategoriBarang.padEnd(10)} | ${harga.toString().padEnd(12)} | ${jumlahStok.toString().padEnd(11)} | ${tanggalMasuk.toString().padEnd(9)} | ${tanggalKadaluarsa.toString().padEnd(13)} | ${distributor.padEnd(12)} | ${lokasiStok.padEnd(11)} | ${deskripsiBarang.padEnd(20)} |`);
+                        });
+                        
+                        console.log(`\n`);
                     }
                     if (Number(invetoryMenu) === 3) {
                         // update stock
+                        console.clear();
+                        console.log(`\n\nHanya ID Barang, Nama Barang, Tanggal Masuk, dan Tanggal Kadaluarsa yang wajib diisi`);
+                        
+                        
+                        let idBarang = prompt(`Enter ID Barang Yang Akan diubah: `);
+                        let namaBarang = prompt(`Enter Nama Barang: `);
+
+                        
+                        // melanjutkan program hanya bila idBarang dan namaBarang tidak kosong
+                        if (idBarang && namaBarang) {
+                            // prompt untuk data opsional
+                            let kategoriBarang = prompt(`Enter Kategori Barang: `);
+                            let harga = prompt(`Enter Harga Barang: `);
+                            let jumlahStok = prompt(`Enter Jumlah Stok: `);
+                            let tanggalMasuk = prompt(`Enter Tanggal Masuk (yyyy-mm-dd): `);
+                            let tanggalKadaluarsa = prompt(`Enter Tanggal Kadaluarsa (yyyy-mm-dd): `);
+                            let distributor = prompt(`Enter Distributor: `);
+                            let lokasiStok = prompt(`Enter Lokasi Stok: `);
+                            let deskripsiBarang = prompt(`Enter Deskripsi: `);
+
+                            // membuat objek untuk parameter yang memiliki nilai
+                            const updatedData = {
+                                idBarang,
+                                namaBarang
+                            };
+    
+                            // menambahkan parameter lainnya jika terdapat nilai
+                            if (kategoriBarang) {
+                                updatedData.kategoriBarang = kategoriBarang;
+                            }
+                            if (harga) {
+                                updatedData.harga = harga;
+                            }
+                            if (jumlahStok) {
+                                updatedData.jumlahStok = jumlahStok;
+                            }
+                            if (tanggalMasuk) {
+                                updatedData.tanggalMasuk = tanggalMasuk;
+                            }
+                            if (tanggalKadaluarsa) {
+                                updatedData.tanggalKadaluarsa = tanggalKadaluarsa;
+                            }
+                            if (distributor) {
+                                updatedData.distributor = distributor;
+                            }
+                            if (lokasiStok) {
+                                updatedData.lokasiStok = lokasiStok;
+                            }
+                            if (deskripsiBarang) {
+                                updatedData.deskripsiBarang = deskripsiBarang;
+                            }
+                            await controller.item.update(updatedData);
+                        } else {
+                            console.log(`ID Barang dan Nama Barang Tidak Boleh Kosong!!`);
+                        }
                     }
                     if (Number(invetoryMenu) === 4) {
                         // delete stock
+                        let idBarang = prompt(`Masukkan id barang yang akan dihapus `);
+                        if (idBarang) {
+                            await controller.item.delete(idBarang).then(console.log(`berhasil dihapus`));
+                        }
                     }
                     if (Number(invetoryMenu) === 5) {
                         // exit
